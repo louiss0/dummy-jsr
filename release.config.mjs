030d782c3f3706056cd140a0f2bed63139697abf
louiss0 <louisshelton0@gmail.com>
2024-07-24 18:04:51 -0400

    chore: place files for necessary files for release

---
+++ release.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ * 2dacbe7b780ddab6cbb9c7e4a939575919fcbf20
+ */
+export default {
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/github",
+	],
+};
